273a577457840dc486f2ad916b377069751c006f
Varun Raj <varunraj82@gmail.com>
2021-01-25 15:46:00 -0500

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Hello There</h1>
+
+  </div>
+);
